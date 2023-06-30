5d5fd1be152249de48a8f7cea216574913d441ae
Mfullana <marcelo.fullana@gmail.com>
2023-06-29 21:00:08 -0300

    Alta Paciente

--- src/main/resources/static/js/pacientes.js
+++ src/main/resources/static/js/pacientes.js
@@ -1,25 +1,35 @@
+var boton = document.getElementById("btn-alta-generica");
+
+boton.addEventListener("click", function (event) {
+ console.log ("hola")
+    {
+        event.preventDefault();
+    }
+});
+
 function altaPaciente() {
     window.open("altaPaciente.html", "popup","width=400,height=940,left=200,top=100");
 }
-function altaGenerico() {
+function altaPacientePost() {
 
     /*Esta funcion es para hacer el alta, funciona y fue probada*/
     console.log('Intentando hacer el alta de ' + document.getElementById("nombre").value + ' ' + document.getElementById("apellido").value)
     //Aqui tomo el mismo Endpoint que aparece en el Swagger//
     const url = 'http://localhost:8080/pacientes/crear';
 
-    let id = parseInt(document.getElementById("id").value)
+//    let id = parseInt(document.getElementById("id").value)
     const settings = {
         method: 'POST',
         body: JSON.stringify({
             nombre: document.getElementById("nombre").value,
             apellido: document.getElementById("apellido").value,
             dni: document.getElementById("dni").value,
-            fechaAlta: document.getElementById("fechaAlta").value.format("YYYY-MM-DD"), //Probar si el format arregla el post
+            fechaAlta: document.getElementById("fechaIngreso").value,
+            domicilio:{
             calle:document.getElementById("calle").value,
             numero:document.getElementById("numero").value,
             localidad:document.getElementById("localidad").value,
-            provincia:document.getElementById("provincia").value,
+            provincia:document.getElementById("provincia").value}
 
         }),
         //Importante agregarle el header para que tome el post
@@ -28,8 +38,18 @@ function altaGenerico() {
         }
     }
 
-    console.log('Alta de ' + document.getElementById("nombre").value + ' ' + document.getElementById("apellido").value + ' exitosa')
+    console.log('Alta de ' + document.getElementById("nombre").value + ' ' +
+                document.getElementById("apellido").value +
+                document.getElementById("dni").value +
+                document.getElementById("fechaIngreso").value +
+                document.getElementById("calle").value +
+                document.getElementById("numero").value +
+                document.getElementById("localidad").value +
+                document.getElementById("provincia").value +
+                ' exitosa')
     //Aca usa el url y la configuracion armada arriba para enviar el post al servidor
+    console.log(settings)
+    console.log(url)
     fetch(url, settings)
         .then(response => response.json())
         .then(data => {
@@ -40,3 +60,4 @@ function altaGenerico() {
         });
 
 }
+
